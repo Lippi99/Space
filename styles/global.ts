ba082ebdc0f606132cb1257a9a10108362897e3d
Felipe Salomao <lipesalomaoclaf@gmail.com>
2022-05-28 15:35:01 -0300

    added variables

--- styles/global.ts
+++ styles/global.ts
@@ -1,7 +1,12 @@
 import { createGlobalStyle } from "styled-components"
 export const GlobalStyle = createGlobalStyle`
 
- 
+  :root {
+    --darker: #000000;
+    --light: #FFFFFF;
+    --gray-600: #8C8C8C;
+  }
+
 * {
     margin: 0;
     padding: 0;
